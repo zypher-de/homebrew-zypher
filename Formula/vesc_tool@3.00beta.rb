0c979435c342029ddaeaebd6e64c2ccd3484fc9d
Marc Hildebrand <zypher@zypher.de>
2020-11-05 22:51:28 +0100

    new version 3.00beta added

--- Formula/vesc_tool@3.00beta.rb
+++ Formula/vesc_tool@3.00beta.rb
@@ -1,10 +1,10 @@
-class VescToolAT207 < Formula
+class VescToolAT300beta < Formula
   desc "This is a GUI written using the Qt toolkit to configure the VESC motor controller"
   homepage "http://vesc-project.com/"
   url "https://github.com/vedderb/vesc_tool.git",
 	:revision => "394c687bb1d305205efb4c61d0bb490a08d177cb"
   head "https://github.com/vedderb/vesc_tool.git"
-  version "2.07"
+  version "3.00beta"
   sha256 ""
   license "GPL-3.0"
 
@@ -15,11 +15,11 @@ class VescToolAT207 < Formula
 	system "qmake -config release 'CONFIG += release_macos build_free'"
 	system "make", "clean"
 	system "make", "-j8"
-    prefix.install "build/macos/vesc_tool_2.07.app"
-	bin.install_symlink "prefix/vesc_tool_2.07.app/Contents/MacOS/vesc_tool_2.07"
+    prefix.install "build/macos/vesc_tool_3.00.app"
+	bin.install_symlink "prefix/vesc_tool_3.00.app/Contents/MacOS/vesc_tool_3.00"
   end
 
   test do
-	system "#{bin}/vesc_tool_2.07", "-h"
+	system "#{bin}/vesc_tool_3.00", "-h"
   end
 end
